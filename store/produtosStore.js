0deae7bf5690a299fc0eb845a3dbcec9a41da434
Maurício Naoki <naokimau@gmail.com>
2024-08-25 23:51:00 -0300

    adição do arquivo de store

---
+++ store/produtosStore.js
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const produtosStore = defineStore("produtos", {
+    //state
+    state() {
+        produtos: []
+    },
+    //action
+    actions: {
+
+    }
+    //getter
+})
